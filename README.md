25e6ffa32565d2c9a0b2e8611f468652c8a733c5
kseretis <serkleanthis@gmail.com>
2022-06-28 11:00:01 +0300

    Testing controller

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ A Demo Fiori application.
 
 ## Application Details
 |               |
-| ------------- |
 |**Application Title**<br>Demo Fiori App|
 |**Module Name**<br>demo-fiori-app|
 |**App Generator**<br>@sap/generator-fiori-freestyle|
--- demo-fiori-app/webapp/controller/View1.controller.js
+++ demo-fiori-app/webapp/controller/View1.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/mvc/XMLView"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, XMLView) {
         "use strict";
+
+        // XMLView.create({
+        //     viewName: "demofioriapp.view.View1"
+        // }).then( function (oView) {
+        //     oView.placeAt("content");
+        // });
 
         return Controller.extend("demofioriapp.controller.View1", {
             onInit: function () {
-
+                //alert("Hello");
+            },
+            onButtonClick: function (){
+                alert("Thanks for clicking");
             }
         });
     });
