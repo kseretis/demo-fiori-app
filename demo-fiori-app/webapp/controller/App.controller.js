7be43fba7cbc27436d09b34cb5a720e11130d3d5
kseretis <serkleanthis@gmail.com>
2022-06-28 14:29:16 +0300

    data model and i18n texts

--- demo-fiori-app/webapp/controller/App.controller.js
+++ demo-fiori-app/webapp/controller/App.controller.js
@@ -1,14 +1,12 @@
-sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("demofioriapp.controller.controller.App", {
-        onInit() {
-        }
-      });
-    }
-  );
-  
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+],
+  function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("demofioriapp.controller.controller.App", {
+      onInit() {
+      }
+    });
+  }
+);
--- demo-fiori-app/webapp/controller/View1.controller.js
+++ demo-fiori-app/webapp/controller/View1.controller.js
@@ -1,27 +1,42 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/mvc/XMLView",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, XMLView, MessageToast) {
+    function (Controller, XMLView, MessageToast, JSONModel, ResourceModel) {
         "use strict";
-
-        // XMLView.create({
-        //     viewName: "demofioriapp.view.View1"
-        // }).then( function (oView) {
-        //     oView.placeAt("content");
-        // });
-
         return Controller.extend("demofioriapp.controller.View1", {
             onInit: function () {
-                //alert("Hello");
+                // data model
+                var oData = {
+                    recipient: {
+                        name: "UI5" 
+                    }
+                };
+                var oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+
+                // set i18n text
+                var i18nModel = new ResourceModel({
+                    bundleName: "demofioriapp.i18n.i18n",
+                    supportedLocales: [""],
+                    fallbackLocale: ""
+                });
+                this.getView().setModel(i18nModel, "i18n");
+
             },
             onButtonClick: function (){
+                // read msg text
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                var sMsg = oBundle.getText("msg", [sRecipient]);
                 //alert("Thanks for clicking");
-                MessageToast.show("Thanks for clicking");
+                MessageToast.show(sMsg);
             }
         });
     });
