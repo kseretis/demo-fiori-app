335c9a8d040b61e9062b0fd8328069abe0a414be
kseretis <serkleanthis@gmail.com>
2022-07-05 17:17:34 +0300

    filtering

--- demo-fiori-app/webapp/controller/InvoiceList.controller.js
+++ demo-fiori-app/webapp/controller/InvoiceList.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "demofioriapp/model/formatter"
+    "demofioriapp/model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
 ], 
-    function(Controller, JSONModel, formatter) {
+    function(Controller, JSONModel, formatter, Filter, FilterOperator) {
     'use strict';
     
     return Controller.extend("demofioriapp.controller.InvoiceList", {
@@ -13,6 +15,16 @@ sap.ui.define([
                 currency: "EUR"
             });
             this.getView().setModel(oViewModel, "list");
+        },
+        onFilterInvoices: function(onEvent) {
+            var filter = [];
+            var query = onEvent.getParameter("query");
+            if (query) 
+                filter.push(new Filter("ProductName", FilterOperator.Contains, query));
+            
+            var list = this.byId("invoiceList");
+            var binding = list.getBinding("items");
+            binding.filter(filter);
         }
     });
 });
