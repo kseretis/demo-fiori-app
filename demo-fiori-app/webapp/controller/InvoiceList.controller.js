57c038336c421fd42b1c52be13469aa1eb8b3930
kseretis <serkleanthis@gmail.com>
2022-07-06 13:32:51 +0300

    View replaced with overview

--- demo-fiori-app/webapp/controller/InvoiceList.controller.js
+++ demo-fiori-app/webapp/controller/InvoiceList.controller.js
@@ -4,27 +4,32 @@ sap.ui.define([
     "demofioriapp/model/formatter",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-], 
-    function(Controller, JSONModel, formatter, Filter, FilterOperator) {
-    'use strict';
-    
-    return Controller.extend("demofioriapp.controller.InvoiceList", {
-        formatter: formatter,
-        onInit: function() {
-            var oViewModel = new JSONModel({
-                currency: "EUR"
-            });
-            this.getView().setModel(oViewModel, "list");
-        },
-        onFilterInvoices: function(onEvent) {
-            var filter = [];
-            var query = onEvent.getParameter("query");
-            if (query) 
-                filter.push(new Filter("ProductName", FilterOperator.Contains, query));
-            
-            var list = this.byId("invoiceList");
-            var binding = list.getBinding("items");
-            binding.filter(filter);
-        }
-    });
-});
+],
+    function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+        'use strict';
+
+        return Controller.extend("demofioriapp.controller.InvoiceList", {
+            formatter: formatter,
+            onInit: function () {
+                var oViewModel = new JSONModel({
+                    currency: "EUR"
+                });
+                this.getView().setModel(oViewModel, "list");
+            },
+            onFilterInvoices: function (onEvent) {
+                var filter = [];
+                var query = onEvent.getParameter("query");
+                if (query)
+                    filter.push(new Filter("ProductName", FilterOperator.Contains, query));
+
+                var list = this.byId("invoiceList");
+                var binding = list.getBinding("items");
+                binding.filter(filter);
+            },
+            onItemClick: function (onEvent) {
+                var router = this.getOwnerComponent().getRouter();
+                router.navTo("details");
+            }
+        });
+    }
+);
